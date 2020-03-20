a3680b3e5630a006b55ddc51e939ff03877df57e
youngkiu <youngkiu@gmail.com>
2020-03-20 13:49:01 +0900

    implement TodoList component

---
+++ app/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View } from 'react-native'
+import Todo from './Todo'
+
+const TodoList = ({ todos }) => {
+    todos = todos.map((todo, i) => {
+        return (
+            <Todo 
+                key={todo.todoIndex} 
+                todo={todo} />
+        )
+    })
+    return (
+        <View>
+            {todos}
+        </View>
+    )
+}
+export default TodoList
